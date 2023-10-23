a2292e2b792c0b9cc53dd2ddbc071f7cb08ca850
phucnv282 <phucnv282@gmail.com>
2023-10-24 00:34:27 +0700

    5: tf vpc by module

---
+++ 5-tf_vpc/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.45.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+module "vpc" {
+  source = "./vpc"
+
+  vpc_cidr_block    = "10.0.0.0/16"
+  private_subnet    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnet     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  availability_zone = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
