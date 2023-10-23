c497851674c0ac074499571ea1bd4c71af0abca3
phucnv282 <phucnv282@gmail.com>
2023-10-23 10:37:01 +0700

    terraform functional programming lesson

---
+++ 3-tf_functional_programming/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "hello" {
+  count         = 5
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+}
+
+output "ec2" {
+  # value = {
+  #   public_ip = [for v in aws_instance.hello : v.public_ip]
+  # }
+  value = { for i, v in aws_instance.hello : format("public_ip%d", i + 1) => v.public_ip }
+}
